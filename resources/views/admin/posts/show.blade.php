f3558a96e72d7ebed713e3f1260ed00dc625113b
bia9400 <ferro.biagio@gmail.com>
2022-08-24 16:30:21 +0200

    end

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app');
 @section('content')
     <div class="container">
-        <h1>{{$post->name}}</h1>
-        <p>{{$post->content}}</p>
-        <a class="btn btn-primary" href="{{route("admin.posts.index")}}">Indietro</a>
-    </div>
+        <h1>{{ $post->name }}</h1>
+        <p>{{ $post->content }}</p>
+        <div>Tags:
+            @foreach ($post->tags as $tag)
+            <span>{{ $tag->name }}</span>
+            @if (!$loop->last)
+                <span> - </span>
+            @endif
+        @endforeach
+        </div>
+        
+        <a class="btn btn-primary" href="{{ route('admin.posts.index') }}">Indietro</a>
 
+    </div>
 @endsection
